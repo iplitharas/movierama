80ed8964a129cb9940cd5f204d657a3fe753c285
iplitharas <ioannisplitharas@improbable.io>
2022-12-24 22:10:14 +0000

    Add test cases for home page, fix type in accounts/login url

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ SITE_ID = 1
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
-LOGIN_URL = "/account/login/"
+LOGIN_URL = "/accounts/login/"
 
 ##################################################################
 
--- tests/web_app/test_home_view.py
+++ tests/web_app/test_home_view.py
@@ -20,10 +20,26 @@ def test_home_page_title(client):
 
 
 @pytest.mark.django_db
-def test_home_with_one_movie(client, fake_user_with_one_movie):
+def test_home_without_a_user_logged(client):
     """
     Given a testing client
-    When I call the home page with one user
+    When I call the `home` page without any user logged in
+    Then I'm expecting the `Login` && `Signup` in the content of
+    the `home` page
+    """
+    url = reverse("home")
+    response = client.get(url)
+    assert response.status_code == http.HTTPStatus.OK
+    assert "Login" in str(response.content)
+    assert "Signup" in str(response.content)
+    assert "hi" not in str(response.content)
+
+
+@pytest.mark.django_db
+def test_home_with_one_movie(client, fake_user_with_one_movie):
+    """
+    Given a testing client one user and one movie
+    When I call the `home` page
     Then I'm expecting the home page with right content
     """
     # Given 1 user with one movie review
@@ -34,28 +50,52 @@ def test_home_with_one_movie(client, fake_user_with_one_movie):
     # Then
     assert response.status_code == http.HTTPStatus.OK
     response_content = str(response.content)
+    # All the movie infos are rendered in the page
     assert movie.title in response_content
     assert movie.desc in response_content
     assert movie.genre not in response_content
     assert movie.year in response_content
+    # User's name is listed as a url
     assert user.username.title() in response_content
-    # Make sure that  users can't add
-    # feedback on their movie reviews
+    # But we aren't able to add any feedback
     assert "Like it" not in response_content
     assert "Dislike it" not in response_content
+    # Or edit/delete the movie
+    assert "Edit" not in response_content
+    assert "Delete" not in response_content
 
 
 @pytest.mark.django_db
-def test_home_without_a_user_logged(client):
+def test_home_with_one_movie_user_logged_in(client, fake_user_with_one_movie):
     """
-    Given a testing client
-    When I call the `home` page without any user logged in
-    Then I'm expecting the `Login` `Signup` in the content of
-    the home page
+    Given a testing client one user and one movie
+    When the user logs in
+    Then I'm expecting the `home page` with right content
     """
+    # Given 1 user with one movie review
+    user, movie = fake_user_with_one_movie
+    url_login = reverse("login")
+    response = client.post(
+        path=url_login, data={"username": user.username, "password": "password123"}
+    )
+    assert response.status_code == http.HTTPStatus.FOUND
+    # When I access the homepage
     url = reverse("home")
     response = client.get(url)
+    # Then
     assert response.status_code == http.HTTPStatus.OK
-    assert "Login" in str(response.content)
-    assert "Signup" in str(response.content)
-    assert "hi" not in str(response.content)
+    response_content = str(response.content)
+    # All the movie infos are rendered in the page
+    assert movie.title in response_content
+    assert movie.desc in response_content
+    assert movie.genre not in response_content
+    assert movie.year in response_content
+    # We can only Logout
+    assert "Hello " + user.username in response_content
+    assert "Logout" in response_content
+    # As the owner of the movie we can't add any feedback
+    assert "Like it" not in response_content
+    assert "Dislike it" not in response_content
+    # But he can edit/delete it
+    assert "Edit" in response_content
+    assert "Delete" in response_content
